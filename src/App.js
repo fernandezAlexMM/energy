4374f045d156cd64e564658730e1a468e5c114bb
fernandezAlex <70136337+fernandezAlex@users.noreply.github.com>
2021-02-16 18:28:51 +0100

    [DEX-3528] component Section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Header } from './components/header/Header';
+import { SectionImg } from './components/sectionimg/SectionImg';
+import data from './data/data'
+
+console.log(data)
+  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header/>
+      <SectionImg 
+        img={data.s1.img} 
+        title={data.s1.title} 
+        content={data.s1.content} 
+        imgPosition={data.s1.imgPosition} 
+      />
+      <SectionImg 
+        img={data.s2.img} 
+        title={data.s2.title} 
+        content={data.s2.content} 
+        imgPosition={data.s2.imgPosition} 
+      />
+      <SectionImg 
+        img={data.s3.img} 
+        title={data.s3.title} 
+        content={data.s3.content} 
+        imgPosition={data.s3.imgPosition} 
+      />
+    </React.Fragment>
   );
 }
 
