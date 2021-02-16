833d6cbabe0bd0560600ef479bb410d6178b849c
fernandezAlex <70136337+fernandezAlex@users.noreply.github.com>
2021-02-16 18:29:18 +0100

    [DEX-3528] component Section

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+const data = {
+  s1:{
+    img:"https://cms-images.mmst.eu/2rj3gcd43pmw/X1s76hIAACQAcE2X/7b1ca0f17647631b0b498811510433c5/Prismic_LP_Visual_Energielabels_1257x703_Teaser_1.jpg?q=80",
+    title: "Energy",
+    content: "Probablemente estés familiarizado con las coloridas etiquetas energéticas que muestra tu última compra de electrodomésticos en Media Markt. Durante más de veinte años, estas etiquetas muestran claramente los datos más importantes de tu electrodoméstico, y con ellas, el desarrollo de la tecnología y de aparatos cada vez más eficientes.<br/>Dado que los dispositivos se han vuelto cada vez más eficientes energéticamente y cumplen con un estándar mucho más alto que el que cubría la Clase A, el concepto actual se ha vuelto menos transparente para todas las partes interesadas. Las diferencias entre A + y A +++ son a veces muy pequeñas, con lo que la UE ha decidido volver a la escala anterior con una nueva etiqueta.  Las que conocemos ahora como A +++, A ++, A +, A, B, C y D, pasarán a ser a partir de marzo de 2021 nuevamente de A a G. Donde A significa dispositivos muy eficientes, que todavía están por salir; los dispositivos B y C identifican los dispositivos más eficientes.",
+    imgPosition: true
+  },
+  s2:{
+    img:"https://cms-images.mmst.eu/2rj3gcd43pmw/X1s76hIAACEAcE2Z/11a59c78ed7f679a757c4aa23d1d5448/Prismic_LP_Visual_Energielabels_1257x703_Teaser_2.jpg?q=80",
+    title: "¿Qué pasará cuando cambie la actual etiqueta en marzo de 2021?	",
+    content: "Solo encontrarás las nuevas etiquetas en tu tienda Media Markt y en la web mediamarkt.es a partir del 1 de marzo de 2021. Durante unos días convivirán las etiquetas nuevas con las anteriores, para que puedas tener una mejor idea de cómo han cambiado las clases. Además, existe una base de datos de información de la UE denominada EPREL, en la que podrá acceder a las nuevas clasificaciones e información adicional no promocional para la mayoría de los dispositivos actualmente disponibles, prevista para el 1 de marzo de 2021. Los fabricantes ya pueden incluir la nueva etiqueta energética con el producto, mientras que los minoristas no pueden mostrar la nueva etiqueta energética antes del 1 de marzo de 2021. Como cliente, por lo tanto, puedes encontrar tanto la etiqueta energética antigua como la nueva durante este período de transición en el electrodoméstico que adquieras.",
+    imgPosition: false
+  },
+  s3:{
+    img:"../img/descripción-etiqueta.png",
+    title: "Así es la etiqueta energética hasta marzo de 2021",
+    content: 'En las etiquetas actuales, además de la clase de eficiencia energética de A +++ a D, según la clase de dispositivo, encuentras información adicional sobre el consumo de energía, consumo de agua, desarrollo de ruido o el volumen de capacidad en la etiqueta. La clasificación se realizó según el grupo de dispositivos de acuerdo con un procedimiento de prueba hasta ahora y que varía  a partir de ahora, marzo del 2021, por lo que es posible que tu frigorífico A +++ actual solo obtenga B, C o incluso D después del nuevo procedimiento de prueba. Esto no significa que tu máquina o aparato pase a ser ineficiente energéticamente sino que varía la forma de evaluarlo. ',
+    imgPosition: true
+  },
+  s4:{
+    img:"https://cms-images.mmst.eu/2rj3gcd43pmw/X1s76hIAACQAcE2X/7b1ca0f17647631b0b498811510433c5/Prismic_LP_Visual_Energielabels_1257x703_Teaser_1.jpg?q=80",
+    title: "test",
+    content: 'I hope you enjoy learning React!',
+    imgPosition: false
+  },
+  s5:{
+    img:"https://cms-images.mmst.eu/2rj3gcd43pmw/X1s76hIAACQAcE2X/7b1ca0f17647631b0b498811510433c5/Prismic_LP_Visual_Energielabels_1257x703_Teaser_1.jpg?q=80",
+    title: "test",
+    content: 'I hope you enjoy learning React!',
+    imgPosition: true
+  }     
+} 
+
+export default data;
