00bc20f64e0c3e998aaf907587294d86eed075a9
fernandezAlex <70136337+fernandezAlex@users.noreply.github.com>
2021-02-17 13:27:30 +0100

    [DEX-3528] new component CardLarge

---
+++ src/components/cardLarge/CardLarge.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const CardLarge = () => {
+  return (
+    <div class="card__large">
+        <img src="https://cms-images.mmst.eu/2rj3gcd43pmw/X19gexIAACMAgpKe/5b5be2c62d20d4dbe5e192e00689db39/Prismic_LP_Visual_Energielabels_3360x887_mood_Waschmaschine.jpg?q=80" alt="Lavadoras"/>  
+        <div class="title">
+            <h3>
+                Lavadoras
+            </h3>
+            <svg width="32" height="32" viewBox="0 0 24 24"><path d="M15.78 11.28a.75.75 0 01.22.53v.38a.77.77 0 01-.22.53l-5.14 5.13a.5.5 0 01-.71 0l-.71-.71a.49.49 0 010-.7L13.67 12 9.22 7.56a.5.5 0 010-.71l.71-.7a.5.5 0 01.71 0z"></path></svg>
+        </div>    
+    </div>
+  );
+};
