f1c023305b339a338641081076affeb78a0d9475
fernandezAlex <70136337+fernandezAlex@users.noreply.github.com>
2021-02-17 13:27:14 +0100

    [DEX-3528] new component CardLarge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Header } from './components/header/Header';
 import { SectionImg } from './components/sectionimg/SectionImg';
+import { CardLarge } from './components/cardLarge/CardLarge';
 import data from './data/data'
 
 console.log(data)
@@ -28,6 +29,19 @@ function App() {
         content={data.s3.content} 
         imgPosition={data.s3.imgPosition} 
       />
+      <SectionImg 
+        img={data.s4.img} 
+        title={data.s4.title} 
+        content={data.s4.content} 
+        imgPosition={data.s4.imgPosition} 
+      />
+      <SectionImg 
+        img={data.s5.img} 
+        title={data.s5.title} 
+        content={data.s5.content} 
+        imgPosition={data.s5.imgPosition} 
+      />
+      <CardLarge />
     </React.Fragment>
   );
 }
